440d6c7d42ce3683a3c3d1440be504ae4cecd58d
CZL <fzyczlluoryao@126.com>
2023-06-05 11:09:18 +0800

    第一次提交

---
+++ Assets/Scripes/GitTestCS.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTestCS : MonoBehaviour
+{
+    private string gitChange = "First";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
